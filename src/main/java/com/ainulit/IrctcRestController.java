21f0c35880324e2a69457c1487140f78480fd168
Ainul <aliainul9568@gmail.com>
2021-07-21 18:38:42 +0530

    add url

--- src/main/java/com/ainulit/IrctcRestController.java
+++ src/main/java/com/ainulit/IrctcRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IrctcRestController {
+       private String url="www.ainulit.com";
 	
 	@GetMapping(value = "/book")
 	public String bookTicket()
